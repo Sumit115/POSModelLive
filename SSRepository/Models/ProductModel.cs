2d017c4f64374e575dfcd8783e6fd61c2631312b
Sumit <Sumit@SUMIT>
2024-06-08 14:29:48 +0530

    SUR

--- SSRepository/Models/ProductModel.cs
+++ SSRepository/Models/ProductModel.cs
@@ -64,6 +64,6 @@ namespace SSRepository.Models
         public string? NameToDisplay { get; set; }//backend =product
         public string? NameToPrint { get; set; }//backend =product
         public string? Image { get; set; }
-        public string? Dome1123 { get; set; }
+        public string? Dome1124 { get; set; }
     }
 }
