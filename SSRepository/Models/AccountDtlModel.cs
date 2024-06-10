76bc2298e407c40e3deff5b6972117b8ef36d3cd
Sumityadav115 <Sumityadav115@gmail.com>
2024-06-10 22:51:53 +0530

    Kissin

--- SSRepository/Models/AccountDtlModel.cs
+++ SSRepository/Models/AccountDtlModel.cs
@@ -30,6 +30,7 @@ namespace SSRepository.Models
         public string? Demo3 { get; set; }
         public string? Demo4 { get; set; }
         public string? Demo5 { get; set; }
+        public string? Demo6 { get; set; }
 
     }
 }
